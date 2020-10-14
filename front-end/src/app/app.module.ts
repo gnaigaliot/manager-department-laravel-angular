e94d51d5e7ceb3d82f654d5583e84e5c0262aedb
GiangTH <hoanggiang521999@gmail.com>
2020-10-15 01:47:46 +0700

    giangth commit create project and config

---
+++ front-end/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SpinnerComponent } from './shared/spinner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SpinnerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
