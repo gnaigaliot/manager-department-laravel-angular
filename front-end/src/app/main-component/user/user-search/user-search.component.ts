871ee33ad608d07ed9080a9bf10659dfe97cf49f
GiangTH <hoanggiang521999@gmail.com>
2020-10-19 01:39:10 +0700

    giangth commit

--- front-end/src/app/main-component/user/user-search/user-search.component.ts
+++ front-end/src/app/main-component/user/user-search/user-search.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BaseComponent } from 'src/app/shared/components/base-component/base-component.component';
 
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
   styleUrls: ['./user-search.component.css']
 })
-export class UserSearchComponent implements OnInit {
+export class UserSearchComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public actr: ActivatedRoute,
+    public router: Router
+  ) {
+    super(actr);
+    this.formSearch = this.buildForm({}, {
+      userCode: ['', [Validators.maxLength(50)]],
+      fullName: ['', [Validators.maxLength(200)]]
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  get f () {
+    return this.formSearch.controls;
+  }
 }
